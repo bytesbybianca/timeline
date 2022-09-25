ec352d4f5495f956da900451cc6a29fba7b35c06
Bianca Togonon <biancatogodev@gmail.com>
2022-09-24 19:44:33 -0700

    Added edit moment feature

--- controllers/moments.js
+++ controllers/moments.js
@@ -2,52 +2,47 @@ const cloudinary = require("../middleware/cloudinary");
 const Moment = require("../models/Moment");
 
 module.exports = {
-  // getProfile: async (req, res) => {
-  //   try {
-  //     const posts = await Post.find({ user: req.user.id });
-  //     res.render("profile.ejs", { posts: posts, user: req.user });
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // },
-  // getTimelines: async (req, res) => {
-  //   try {
-  //     const timelines = await Timeline.find().sort({ createdAt: "desc" }).lean();
-  //     res.render("timelines.ejs", { timelines: timelines });
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // },
-  // getProject: async (req, res) => {
-  //   try {
-  //     const project = await Timeline.findById(req.params.projectId);
-  //     res.render("project.ejs", { project: project, user: req.user });
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // },
-  createMoment: async (req, res) => {
+  editMoment: async (req, res) => {
+    const moment = await Moment.findById(req.params.momentId);
+
     try {
-      // Upload image to cloudinary
-      // const result = await cloudinary.uploader.upload(req.file.path);
-      // const project = await Timeline.findById(req.params.projectId);
-      
-      await Moment.create({
-        momentType: req.body.type,
-        // momentImage: result.secure_url,
-        // cloudinaryId: result.public_id,
-        location: req.body.location,
-        tweetId: req.body.tweetId,
-        journalEntry: req.body.journal,
-        date: req.body.date,
-        time: req.body.time,
-        user: req.user.id,
-      });
-      
-      // console.log(req);
-      console.log("A new moment has been created!");
-      // TO-DO: REDIRECT TO PROJECT ID
-      res.redirect("/timelines");
+      // update journal
+      if(moment.momentType === 'journal') {
+        await Moment.findOneAndUpdate(
+          { _id: moment._id },
+          {
+            date: req.body.date,
+            journalEntry: req.body.journal,
+          }
+        );     
+      }
+      // update location
+      if(moment.momentType === 'location') {
+        await Moment.findOneAndUpdate(
+          { _id: moment._id },
+          {
+            date: req.body.date,
+            location: req.body.location,
+          }
+        );     
+      }
+      // update photo
+      if(moment.momentType === 'photo') {
+        const result = await cloudinary.uploader.upload(req.file.path);
+        const secure_url = result.secure_url;
+        const public_id = result.public_id;
+
+        await Moment.findOneAndUpdate(
+          { _id: moment._id },
+          {
+            date: req.body.date,
+            momentImage: result.secure_url,
+            cloudinaryId: result.public_id,
+          }
+        );     
+      }
+      console.log("Moment updated");
+      res.redirect(`/timelines/${moment.timelineProject}`);
     } catch (err) {
       console.log(err);
     }
